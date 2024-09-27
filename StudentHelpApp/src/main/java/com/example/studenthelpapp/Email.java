cf5434a8730dad060abc605804603ca1041f5f50
Andrew Null <andrew@nulls.us>
2024-09-27 15:45:31 -0700

    Email Class

---
+++ StudentHelpApp/src/main/java/com/example/studenthelpapp/Email.java
@@ -0,0 +1,21 @@
+public class Email {
+    String username;
+    String domain;
+
+    public static Email make_email(String input){
+        int at = input.indexOf('@');
+        if (input.indexOf('@', at + 1) != -1) {return null;}
+        //substring: [)
+        return new Email(
+            input.substring(0, at),
+            input.substring(at+1, input.length())
+        );
+    }
+
+    private Email(String u, String d) {
+        this.username = u;
+        this.domain = d;
+    }
+
+    public String to_string() {return this.username + "@" + this.domain;}
+}
