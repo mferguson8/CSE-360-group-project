fa6bf3e186ba9115ef736a96db90d015233bbe3d
Andrew Null <andrew@nulls.us>
2024-09-27 15:01:04 -0700

    No FXML

--- StudentHelpApp/src/main/java/com/example/studenthelpapp/HelloApplication.java
+++ StudentHelpApp/src/main/java/com/example/studenthelpapp/HelloApplication.java
@@ -1,23 +1,35 @@
-package com.example.studenthelpapp;
+//package com.example.studenthelpapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    public void start(Stage primaryStage) {
+    	System.out.println("ASU Hello World!");
+    	System.out.println("It started!");
+        primaryStage.setTitle("ASU Hello World Spring 2024");
+        Button btn = new Button();
+        btn.setText("Display: 'ASU says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("ASU: Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
