26178abecf81f8a794018730ccd8610398016c6d
Anthony Cardin <anthonycardin@Anthonys-MacBook-Pro.local>
2024-10-06 13:38:41 -0700

    commit message

--- StudentHelpApp/src/main/java/module-info.java
+++ StudentHelpApp/src/main/java/module-info.java
@@ -2,11 +2,15 @@ module com.example.studenthelpapp {
     requires javafx.controls;
     requires javafx.fxml;
 <<<<<<< HEAD
+<<<<<<< HEAD
     requires java.sql;
 
 =======
     
 >>>>>>> 081d4a2 (fixed maven / java issues)
+=======
+
+>>>>>>> 18cf93c (my changes)
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
