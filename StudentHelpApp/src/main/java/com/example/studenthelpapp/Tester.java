e56b4079e3e51c1062f85aaaeb0d104bac65d075
Andrew Null <andrew@nulls.us>
2024-10-09 15:52:12 -0700

    Second Email Test

--- StudentHelpApp/src/main/java/com/example/studenthelpapp/Tester.java
+++ StudentHelpApp/src/main/java/com/example/studenthelpapp/Tester.java
@@ -11,11 +11,27 @@ public class Tester {
 
     }
 
+    private static void runTest(boolean result, String TID, String success, String fail) {
+        testsRun += 1;
+        if (result) {
+            testsSuccess += 1;
+            System.out.println(TID + ": "+ success);
+        }
+        else System.out.println(TID + ":  " + fail);
+    }
+
+    /* RUN TESTS */
     private static void testEmail() {
-        runTest(tE1_Valid(), "tE1", "Can create a valid email", "can't create a valid email");
+        runTest(tE1_Valid(), "tE1",
+                "Can create a valid email", "can't create a valid email");
+        runTest(tE2_OneAtOnly(), "tE2",
+                "Can't create an email with multiple @s", "¿can? create an email with multiple @s");
+        //runTest(tE3_OneAtOnly(), "tE3",
+        //        "Can't create an email with multiple @s", "¿can? create an email with multiple @s");
 
     }
 
+    /* TESTS */
     private static boolean tE1_Valid() {
         final String testStr = "test@test.test";
         Email.EmailResult er = Email.make_email(testStr);
@@ -24,13 +40,19 @@ public class Tester {
         return result && (e.toString().equals(testStr));
     }
 
-    private static void runTest(boolean result, String TID, String success, String fail) {
-        testsRun += 1;
-        if (result) {
-            testsSuccess += 1;
-            System.out.println(TID + ": "+ success);
-        }
-        else System.out.println(TID + ":  " + fail);
+    private static boolean tE2_OneAtOnly() {
+        final String testStr = "test@test@test.test";
+        Email.EmailResult er = Email.make_email(testStr);
+        Email.EmailResult.Status erc = er.check();
+        if (erc == Email.EmailResult.Status.MULT_ATS) return true;
 
+        //System.out.print("\t");
+        //switch (erc) {
+        //    case NO_DOM:
+        //}
+        return false;
     }
+
+
+
 }
